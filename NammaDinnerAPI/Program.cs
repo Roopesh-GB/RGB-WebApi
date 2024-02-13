127ed1a25d7f67b59250aacc05a84768bb6bfe39
Roopesh G B <roopeshgb3@gmail.com>
2024-02-13 22:24:50 +0530

    Fixing the issue with localhost run

--- NammaDinnerAPI/Program.cs
+++ NammaDinnerAPI/Program.cs
@@ -1,4 +1,7 @@
+using NammaDinnerAPI.Helper;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Fixing the issue with DateTime in .Net 6
+builder.Services.AddControllers()
+               .AddJsonOptions(options =>
+               {
+                   options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+               });
 
 var app = builder.Build();
 
